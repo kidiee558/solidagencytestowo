1db707b8e04ea66d20a4205e4ef10d197c0bcdf5
kidiee558 <kidiee558@gmail.com>
2026-03-27 13:09:22 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import FluidBackground from "./components/FluidBackground";
+
+export default function App() {
+  return (
+    <main className="relative w-full h-screen overflow-hidden">
+      <FluidBackground />
+    </main>
+  );
+}
