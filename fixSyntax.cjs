0a2dd322c4c702fe0335080b373f7e56fb677e26
kidiee558 <kidiee558@gmail.com>
2026-04-19 21:55:26 +0200

    Delete fixSyntax.cjs

--- fixSyntax.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-
-// Fix the poster injection error: The regex matched the closing bracket `/>` and appended the poster wrong.
-code = code.replace(/\/ poster=/g, 'poster=');
-
-fs.writeFileSync('src/App.tsx', code);
